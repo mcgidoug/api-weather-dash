e9434fda0fbb47290353146c3558e7f70f8fde60
mcgidoug <mcgidoug@gmail.com>
2023-01-13 15:30:43 -0600

    re-commit (removed api key)

---
+++ assets/js/app.js
@@ -0,0 +1,41 @@
+// declare weather api functions within weather variable
+let weather = {
+  // api key (add your own key here)
+  apiKey: YOUR_API_KEY,
+  // fetch weather function - fetch from open weather with city input
+  fetchWeather: function (city) {
+    fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+        city +
+        "&units=metric&appid=" +
+        this.apiKey
+    )
+      .then((response) => response.json())
+      .then((data) => this.weatherFunction(data));
+  },
+  // sorting through fetched data to retrieve city weather info
+  weatherFunction: function (data) {
+    let { name } = data;
+    let { icon, description } = data.weather[0];
+    let { temp, humidity } = data.main;
+    let { speed } = data.wind;
+    // query selectors referencing classes on html to insert api weather data
+    document.querySelector(".cityName").innerText = "Weather in " + name;
+    document.querySelector(".icon").src =
+      "https://openweathermap.org/img/wn/" + icon + "@2x.png";
+    document.querySelector(".description").innerText = description;
+    document.querySelector(".temperature").innerText = temp + "°C";
+    document.querySelector(".humidity").innerText =
+      "Humidity: " + humidity + "%";
+    document.querySelector(".windSpeed").innerText =
+      "Wind Speed: " + speed + "km/h";
+  },
+  // target searchbar value - use for input value of weather api
+  searchCity: function () {
+    this.fetchWeather(document.querySelector(".search-bar").value);
+  },
+};
+// click search to find weather
+document.querySelector(".search button").addEventListener("click", function () {
+  weather.searchCity();
+});
---
+++ readme.md
@@ -0,0 +1,12 @@
+## Description
+
+(NO LONGER HOSTED) A simple weather dashboard that allows user input of city and retrieves weather data based on input. Feel free to clone and add your own api key to the app.js file.
+
+## Screenshot
+
+![Screenshot of page](./weather-dashboard-screenshot.png)
+
+## Works Cited
+
+Used YouTube tutorial to write weather retrieval function.
+https://www.youtube.com/watch?v=WZNG8UomjSI&t=1814s
